c1420c86ef681dcdcf4ffb6e92d0a7440e6ab5bf
PWJ <2221395968@qq.com>
2019-07-14 14:43:55 +0800

    change the project struct of Spring using JDBC
    complete spring upload file task，but no datails， just code

--- src/main/java/com/pwj/Application.java
+++ src/main/java/com/pwj/Application.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@ComponentScan
 public class Application {
 
     public static void main(String[] args) {
