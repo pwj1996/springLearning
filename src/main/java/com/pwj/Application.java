532b781eb955e9bad216df864bf7dabb03a8c7e0
HKong <hao004@e.ntu.edu.sg>
2019-07-16 20:51:07 +0800

    Complete: Authenticating a User with LDAP

--- src/main/java/com/pwj/Application.java
+++ src/main/java/com/pwj/Application.java
@@ -1,11 +1,9 @@
 package com.pwj;
 
 import com.pwj.component.StorageProperties;
-import com.pwj.service.StorageService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
---
+++ src/main/java/com/pwj/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.pwj.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String index() {
+        return "Welcome to the home page!";
+    }
+}
